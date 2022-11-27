e192e49d45bfd8fbc139e01187769311d24816f7
Stefan Genov <stefantigro@gmail.com>
2022-11-27 13:37:30 +0200

    [master] Added Terraform configuration for my existing resources

---
+++ Terraform/.terraform/providers/registry.terraform.io/telmate/proxmox/2.9.11/linux_amd64/README.md
@@ -0,0 +1,53 @@
+[![Build Status](https://travis-ci.com/Telmate/terraform-provider-proxmox.svg?branch=master)](https://travis-ci.com/Telmate/terraform-provider-proxmox)
+
+# Terraform provider plugin for Proxmox
+
+This repository provides a Terraform provider for
+the [Proxmox virtualization platform](https://pve.proxmox.com/pve-docs/) and exposes Terraform resources to provision
+QEMU VMs and LXC Containers.
+
+## Getting Started
+
+In order to get started, use [the documentation included in this repository](docs/index.md). The documentation contains
+a list of the options for the provider. Moreover, there are some guides available how to combine options and start
+specific VMs.
+
+## Quick Start
+
+Follow this [install guide](docs/guides/installation.md) to install the plugin.
+
+## Known Limitations
+
+* `proxmox_vm_qemu`.`disk`.`size` attribute does not match what is displayed in the Proxmox UI.
+* Updates to `proxmox_vm_qemu` resources almost always result as a failed task within the Proxmox UI. This appears to be
+  harmless and the desired configuration changes do get applied.
+* `proxmox_vm_qemu` does not (yet) validate vm names, be sure to only use alphanumeric and dashes otherwise you may get
+  an opaque 400 Parameter Verification failed (indicating a bad value was sent to proxmox).
+* When using the `proxmox_lxc` resource, the provider will crash unless `rootfs` is defined.
+* When using the Network Boot mode (PXE), a valid NIC must be defined for the VM, and the boot order must specify network first.
+
+## Contributing
+
+When contributing, please also add documentation to help other users.
+
+### Debugging the provider
+
+Debugging is available for this provider through the Terraform Plugin SDK versions 2.0.0. Therefore, the plugin can be
+started with the debugging flag `--debug`.
+
+For example (using [delve](https://github.com/go-delve/delve) as Debugger):
+
+```bash
+dlv exec --headless ./terraform-provider-my-provider -- --debug
+```
+
+For more information about debugging a provider please
+see: [Debugger-Based Debugging](https://www.terraform.io/docs/extend/debugging.html#debugger-based-debugging)
+
+## Useful links
+
+* [Proxmox](https://www.proxmox.com/en/)
+* [Proxmox documentation](https://pve.proxmox.com/pve-docs/)
+* [Terraform](https://www.terraform.io/)
+* [Terraform documentation](https://www.terraform.io/docs/index.html)
+* [Recommended ISO builder](https://github.com/Telmate/terraform-ubuntu-proxmox-iso)
--- Terraform/homeLab.tf
+++ Terraform/homeLab.tf
@@ -24,28 +24,138 @@ provider "proxmox" {
   pm_api_token_secret = var.pm_api_token_secret
 }
 
-resource "proxmox_vm_qemu" "resource-name" {
+# A lot of extra parameters passed, since we are configuring already existing vms :) If I ever redo my cluster, can be simplified!
+
+resource "proxmox_vm_qemu" "k3s-v1" {
   name        = "k3s-v1"
   vmid        = 100
   target_node = "prox-1"
-#  iso         = "local:iso/ubuntu-22.04-live-server-amd64.iso"
 
   cores       = 2
   sockets     = 2
   memory      = 11500
 
-
   disk {
-    // This disk will become scsi0
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "64G"
-    //<arguments omitted for brevity...>
+    type          = "scsi"
+    storage       = "local-lvm"
+    size          = "64G"
+    volume        = "local-lvm:vm-100-disk-0"
   }
 
   network{
     model     = "virtio"
     bridge    = "vmbr0"
     firewall  = true
+    macaddr   = "22:F9:B7:37:2E:DE"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, desc
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k3s-v2" {
+  name        = "k3s-v2"
+  vmid        = 101
+  target_node = "prox-1"
+
+  cores       = 2
+  sockets     = 2
+  memory      = 11500
+
+  disk {
+    type          = "scsi"
+    storage       = "local-lvm"
+    size          = "64G"
+    volume        = "local-lvm:vm-101-disk-0"
+  }
+
+  network{
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = true
+    macaddr   = "0E:D5:B3:62:30:01"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, desc
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k3s-v3" {
+  name        = "k3s-v3"
+  vmid        = 102
+  target_node = "prox-2"
+
+  cores       = 2
+  sockets     = 2
+  memory      = 11500
+
+  disk {
+    type          = "scsi"
+    storage       = "local-lvm"
+    size          = "64G"
+    volume        = "local-lvm:vm-102-disk-0"
+  }
+
+  disk {
+    type          = "scsi"
+    storage       = "extra"
+    size          = "220G"
+    volume        = "extra:vm-102-disk-0"
+  }
+
+  network{
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = true
+    macaddr   = "FE:20:33:30:62:9D"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, desc
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k3s-v4" {
+  name        = "k3s-v4"
+  vmid        = 103
+  target_node = "prox-2"
+
+  cores       = 2
+  sockets     = 2
+  memory      = 11500
+
+  disk {
+    type          = "scsi"
+    storage       = "local-lvm"
+    size          = "64G"
+    volume        = "local-lvm:vm-103-disk-0"
+  }
+
+  disk {
+    type          = "scsi"
+    storage       = "extra"
+    size          = "220G"
+    volume        = "extra:vm-103-disk-0"
+  }
+
+  network{
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = true
+    macaddr   = "92:CC:0F:7C:89:DA"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, desc
+    ]
   }
 }
